8bd21eb6de409e72768bf287e504d5dae3913990
Dominique Broeglin <dominique.broeglin@microsoft.com>
2025-04-28 13:49:46 +0200

    Added CLI code

---
+++ az_ai/ingestion/app.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from typing import Annotated
+
+import typer
+from rich.console import Console
+
+from az_ai.ingestion.schema import FragmentSelector
+from az_ai.ingestion.repository import LocalRepository
+from az_ai.ingestion.tools.rich import fragment_as_table
+
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.command()
+def show(
+    repository: Annotated[Path, typer.Option(help="Path to the repository.")],
+    type: Annotated[str | None, typer.Option(help="Filter only fragment of this type.")] = None,
+    label: Annotated[str | None, typer.Option(help="Filter only fragment of with this label.")] = None,
+):
+    console = Console()
+    repository = LocalRepository(path=repository)
+    if type or label:
+        fragment_type = "Fragment" if type is None else type
+        spec = FragmentSelector(fragment_type=fragment_type, label=label)
+        result = repository.find(spec)
+    else: 
+        result = repository.find()
+
+    for fragment in result:
+        console.print(fragment_as_table(fragment))
+
+@app.command()
+def human(
+    repository: Annotated[Path, typer.Option(help="Path to the repository.")],
+):
+    repository = LocalRepository(path=repository)
+
+    for path in sorted(repository.human_path().glob("**/*")):
+        if path.is_file():
+            print(path)
+
+
+app()
--- az_ai/ingestion/tools/rich.py
+++ az_ai/ingestion/tools/rich.py
@@ -5,6 +5,7 @@ from rich.panel import Panel
 from rich.rule import Rule
 from rich.table import Table, box, Column
 from rich.text import Text
+from rich.markdown import Markdown
 
 
 from az_ai.ingestion.schema import Fragment
@@ -15,12 +16,16 @@ def fragment_as_table(fragment: Fragment) -> Panel:
     Convert a Fragment to a Rich Panel.
     """
 
-    border_style = "white"
-    metadata_table = Table(Column("Name"), Column("Value", no_wrap=True), box=box.SIMPLE_HEAD)
+    metadata_table = Table(Column("Name", no_wrap=True), Column("Value", no_wrap=True), box=box.SIMPLE_HEAD)
     relationships_table = Table(
         "Type", "Target", box=box.SIMPLE_HEAD
     )
     content_table = Table(box=box.SIMPLE_HEAD, show_header=False)
+
+    tables = Table.grid()
+    tables.add_column()
+    tables.add_column()
+    tables.add_row(metadata_table, relationships_table)
 
     # for rel_type, target in node.relationships.items():
     #     target_text = f"{target.node_type}\nNode ID: {target.node_id}\nFile name: {target.metadata.get('file_name', 'no filename')}"
@@ -29,28 +34,50 @@ def fragment_as_table(fragment: Fragment) -> Panel:
     relationships_table.add_row("Source", "Dummy")
 
     for key, value in fragment.metadata.items():
-        value_str = escape(str(value))
+        value_str = str(value)
+        value_str = escape(value_str[:100] + ("…" if len(value_str) > 100 else ""))
         metadata_table.add_row(
-            Text(key), Text(value_str, overflow="ellipsis") # value_str[:30] + ("…" if len(value_str) > 30 else ""), 
+            Text(key), Text(value_str, overflow="ellipsis") 
         )
 
     content_table.add_row(
-        "content size", str(len(fragment.content)) if fragment.content else "0"
+        "Content size", str(len(fragment.content)) if fragment.content else "0"
     )
     content_table.add_row("mime type", fragment.mime_type)
 
-    content = "Text placeholder"
+    match fragment.class_name():
+        case "Chunk":
+            panel_box = box.HEAVY
+        case "Document":
+            panel_box = box.DOUBLE
+        case _:
+            panel_box = box.SQUARE
+
+    if fragment.content is None:
+        content = Text("Content is empty...", style="italic")
+    else:
+        match fragment.mime_type:
+            case "text/markdown":
+                content = fragment.content.decode("utf-8").splitlines()
+                content = (content[:50] + ["\n__Truncated...__"]) if len(content) > 50 else content
+                content = Markdown("\n".join(content))
+            case "text/plain":
+                content = Text(escape(fragment.content.decode("utf-8")), overflow="ellipsis")
+            case mime if mime.startswith("image/"):
+                content= Text(f"{mime} image placeholder...", style="italic")
+            case _:
+                content= Text(f"Unsupported {fragment.mime_type} placeholder...", style="italic")
+
 
     panel = Panel(
         Group(
             content,
-            Rule(style=border_style, title="Content Details"),
+            Rule(title="Content Details", style="white"),
             content_table,
-            Rule(style=border_style, title="Metadata & Relationships"),
-            metadata_table,
-            relationships_table,
+            Rule(title="Metadata & Relationships", style="white"),
+            tables,
         ),
-        title=escape(str(fragment)),
-        border_style=border_style,
+        box=panel_box,
+        title=f"[bold blue]{escape(str(fragment))}[/]",
     )
     return panel
