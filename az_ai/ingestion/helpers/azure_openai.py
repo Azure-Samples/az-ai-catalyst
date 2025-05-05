d389fa563202a6a554a770330363a0420a51edb3
Dominique Broeglin <dominique.broeglin@microsoft.com>
2025-05-05 11:19:08 +0200

    Added documentation for embeddings tool

--- az_ai/ingestion/helpers/azure_openai.py
+++ az_ai/ingestion/helpers/azure_openai.py
@@ -4,9 +4,19 @@ from az_ai.ingestion.ingestion import Ingestion
 from az_ai.ingestion.schema import Chunk, Fragment
 
 
-def create_embeddings(ingestion: Ingestion, model: str, fragment: Fragment, **kwargs: dict[str, Any]) -> list[float]:
+def create_embeddings(ingestion: Ingestion, model: str, fragment: Fragment, **kwargs: dict[str, Any]) -> Chunk:
     """
-    Create embeddings for all fragments in the repository.
+    Create embeddings for the fragment.
+
+    Args:
+        ingestion (Ingestion): The ingestion instance.
+        model (str): The model to use for embeddings.
+        fragment (Fragment): The fragment to create embeddings for.
+        **kwargs: Additional arguments that will be passed to Chunk.with_source()
+
+    Returns:
+        Chunk: A new Chunk instance with the source set to the fragment, the vector set to the embeddings, 
+        content set to the fragment's content and kwargs applied.
     """
     response = ingestion.azure_openai_client.embeddings.create(
                 input=fragment.content_as_str(),
